6e6095b21fe82102b5ebe87a8c4a938f2e2bd7e3
Invisible1nk <simpson7257@gmail.com>
2025-03-12 21:48:11 +0900

    merge
    .

--- Assets/03.Scripts/Dot/DotController.cs
+++ Assets/03.Scripts/Dot/DotController.cs
@@ -159,7 +159,7 @@ public class DotController : MonoBehaviour
     {
         Debug.Log("스테이트:" + State);
         Debug.Log("GetSubSCript");
-        if (manager.Pattern == GamePatternState.MainA || manager.Pattern == GamePatternState.MainB || manager.Pattern == GamePatternState.Play || manager.Pattern == GamePatternState.Sleeping)
+        if (manager.Pattern == GamePatternState.MainA || manager.Pattern == GamePatternState.MainB || manager.Pattern == GamePatternState.Play || manager.Pattern == GamePatternState.Sleeping || manager.Pattern == GamePatternState.NextChapter)
         {
             return 0;
         }
