d64f5db5f4adfb13cc5ffcefc71bcd7b1b1d1daf
lodestar123 <lodestar2134@gmail.com>
2025-07-19 01:52:11 +0900

    로딩 구현 진행
    LoadingScene, LoadingController.cs 작성, IntroScene.cs 수정

--- Assets/03.Scripts/UI/LoadingController.cs
+++ Assets/03.Scripts/UI/LoadingController.cs
@@ -2,27 +2,80 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadingController : MonoBehaviour
 {
-    [SerializeField]
-    TMP_Text title;
-    [SerializeField]
-    TMP_Text dailytips;
+    public static LoadingController Instance { get; private set; }
 
     [SerializeField]
-    PlayerController pc;
-    int currentChapter;
-    // Start is called before the first frame update
-    void Start()
+    private TextMeshProUGUI textChapter;
+    private TextMeshProUGUI textDailyTips;
+
+    private AsyncOperation operation;
+    private string targetSceneName;
+    private int currentChapter;
+
+    public GameObject defaultLoadingScreen;
+    public GameObject chapterLoadingScreen;
+
+    void Awake()
     {
-        if(pc)
-        { 
-            currentChapter = pc.GetChapter();
-            int lang = (int)pc.GetLanguage();
-            title.text = DataManager.Instance.ChapterList.chapters[currentChapter].title[lang];
-            dailytips.text = DataManager.Instance.ChapterList.chapters[currentChapter].loadText[lang];
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
         }
     }
 
+    public static void LoadScene(string sceneName, int chapter = 0)
+    {
+        if (Instance == null)
+        {
+            Debug.LogError("LoadingController instance is not initialized.");
+            return;
+        }
+
+        Instance.targetSceneName = sceneName;
+        Instance.currentChapter = chapter;
+        Debug.Log($"Scene Loading: {sceneName}");
+        SceneManager.LoadScene("LoadingScene");
+
+        //StartCoroutine(LoadSceneCoroutine(sceneName));
+    }
+
+    void OnEnable()
+    {
+        if (!string.IsNullOrEmpty(targetSceneName) && SceneManager.GetActiveScene().name == "LoadingScene")
+        {
+            if (currentChapter > 0)
+            {
+                chapterLoadingScreen.SetActive(true);
+                defaultLoadingScreen.SetActive(false);
+                //textChapter.text = $"Chapter {currentChapter}";
+            }
+            else
+            {
+                chapterLoadingScreen.SetActive(false);
+                defaultLoadingScreen.SetActive(true);
+            }
+            StartCoroutine(LoadSceneCoroutine(targetSceneName));
+        }
+    }
+
+    IEnumerator LoadSceneCoroutine(string sceneName)
+    {
+        operation = SceneManager.LoadSceneAsync(sceneName);
+        operation.allowSceneActivation = true;
+
+        while (!operation.isDone)
+        {
+
+            yield return null;
+        }
+    }
 }
