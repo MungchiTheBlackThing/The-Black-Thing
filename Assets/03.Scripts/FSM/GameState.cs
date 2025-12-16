3d0fb2ab89b1b64eb5bea07d420db9de5394eee9
Jiwon <jiwon000512@icloud.com>
2025-12-16 23:20:29 +0900

    유니티 버전 업그레이드 2022.3.54.f1

--- Assets/03.Scripts/FSM/GameState.cs
+++ Assets/03.Scripts/FSM/GameState.cs
@@ -1,5 +1,4 @@
 using Assets.Script.DialClass;
-using Mono.Cecil;
 using System;
 using System.Collections;
 using System.Collections.Generic;
--- Assets/03.Scripts/Manager/ObjectManager.cs
+++ Assets/03.Scripts/Manager/ObjectManager.cs
@@ -1,5 +1,4 @@
 using Assets.Script.TimeEnum;
-using Mono.Cecil.Cil;
 using System;
 using System.Collections;
 using System.Collections.Generic;
