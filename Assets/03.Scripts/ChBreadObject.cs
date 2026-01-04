628808f836b4781eac488c398bbec62a05ff6035
andiemaelee <151032205+andiemaelee@users.noreply.github.com>
2026-01-04 23:59:23 +0900

    달 송신기 나가기 버튼에서 팝업 안 뜨던 문제 보완
    달 송신기 나가기 버튼에서 팝업 안 뜨던 문제 보완

--- Assets/03.Scripts/ChBreadObject.cs
+++ Assets/03.Scripts/ChBreadObject.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class ChBreadObject : MonoBehaviour
 {
     // ObjectManager에서 호출
-    public bool ShouldBeActive(bool isDay8Completed)
+    public bool ShouldBeActive(bool isDay8SleepEventCompleted)
     {
         // 성공 전(false) → true 반환 (보임)
         // 성공 후(true)  → false 반환 (안 보임)
-        return !isDay8Completed;
+        return !isDay8SleepEventCompleted;
     }
 }
