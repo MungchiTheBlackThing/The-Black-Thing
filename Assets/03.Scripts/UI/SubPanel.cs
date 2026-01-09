2064b09e779761e6aa5f412df31a372f8c6bdcc0
andiemaelee <151032205+andiemaelee@users.noreply.github.com>
2026-01-09 17:02:32 +0900

    서브 말풍선 뜨는 시간
    서브 말풍선 뜨는 시간

--- Assets/03.Scripts/UI/SubPanel.cs
+++ Assets/03.Scripts/UI/SubPanel.cs
@@ -523,7 +523,7 @@ public class SubPanel : MonoBehaviour
         // 단일 버튼(넘김 버튼)만 페이드할 때는 button 전달
         var btn = subClick ? subClick.GetComponent<Button>() : null;
         //[DEBUG] 0.5f -> 0.01f
-        StartCoroutine(FadeIn(selectedDot.GetComponent<CanvasGroup>(), 0.01f, btn));
+        StartCoroutine(FadeIn(selectedDot.GetComponent<CanvasGroup>(), 0.5f, btn));
         if (btn) RegisterNextButton(btn);
     }
 
@@ -538,7 +538,7 @@ public class SubPanel : MonoBehaviour
             btn = selectedDot.transform.GetChild(0).GetChild(1).GetComponent<Button>();
         }
         //[DEBUG] 0.5f -> 0.01f
-        StartCoroutine(FadeIn(selectedDot.GetComponent<CanvasGroup>(), 0.01f, btn));
+        StartCoroutine(FadeIn(selectedDot.GetComponent<CanvasGroup>(), 0.5f, btn));
         if (btn) RegisterNextButton(btn);
     }
 
